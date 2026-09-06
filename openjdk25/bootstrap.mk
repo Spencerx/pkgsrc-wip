0e1fbaf605e1d8b5e2202d483961ce7ad5adcbed
Tobias Nygren <tnn@NetBSD.org>
2026-09-06 21:31:42 +0200

    openjdk25: add proper Linux support
    Will make it easier to maintain bootstrap kits.

--- openjdk25/bootstrap.mk
+++ openjdk25/bootstrap.mk
@@ -8,8 +8,15 @@ PKG_FAIL_REASON+=		"Only supports NetBSD >= 9"
 PKG_FAIL_REASON+=		"Only supports NetBSD >= 9.4"
 .endif
 
-# FIXME add new boostrap kits here.
+ONLY_FOR_PLATFORM+=		Linux-*-x86_64
+BOOT.linux-amd64=		OpenJDK25U-jdk_x64_linux_hotspot_25.0.4.1_1.tar.gz
+SITES.${BOOT.linux-amd64}=	https://github.com/adoptium/temurin25-binaries/releases/download/jdk-25.0.4.1%2B1/
+.if !empty(MACHINE_PLATFORM:MLinux-*-x86_64) || make(distinfo)
+DISTFILES+=			${BOOT.linux-amd64}
+EXTRACT_ONLY+=			${BOOT.linux-amd64}
+ALT_BOOTDIR=			${WRKDIR}/jdk-25.0.4.1+1
+.endif
 
 CONFIGURE_ENV+=		LD_LIBRARY_PATH=${ALT_BOOTDIR}/lib
 
-ALT_BOOTDIR=		${WRKDIR}/bootstrap
+ALT_BOOTDIR?=		${WRKDIR}/bootstrap
